1fd17be3d6c2a41f1a8017387ed1535d82bfaad5
devhazemaq <hazemahmedqindeel@gmail.com>
2023-10-02 07:36:53 +0300

    Add more items in sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,4 @@
-import { Home } from "@mui/icons-material";
+import { AccountBox, Article, Group, Home, Person, Settings, Storefront,ModeNight } from "@mui/icons-material";
 import {
   Box,
   List,
@@ -6,6 +6,7 @@ import {
   ListItemButton,
   ListItemIcon,
   ListItemText,
+  Switch,
 } from "@mui/material";
 import React from "react";
 
@@ -19,23 +20,78 @@ const Sidebar = () => {
     >
       <List>
         <ListItem disablePadding>
-          <ListItemButton>
+          <ListItemButton component="a" href="#simple-list" >
             <ListItemIcon>
               <Home />
             </ListItemIcon>
-            <ListItemText primary="Inbox" />
+            <ListItemText primary="Homepage" />
           </ListItemButton>
         </ListItem>
+        {/* -----<>------ */}
+        <ListItem disablePadding>
+          <ListItemButton component="a" href="#simple-list" >
+            <ListItemIcon>
+              <Article />
+            </ListItemIcon>
+            <ListItemText primary="Pages" />
+          </ListItemButton>
+        </ListItem>
+        {/* -----<>------ */}
+        <ListItem disablePadding>
+          <ListItemButton component="a" href="#simple-list" >
+            <ListItemIcon>
+              <Group />
+            </ListItemIcon>
+            <ListItemText primary="Groups" />
+          </ListItemButton>
+        </ListItem>
+        {/* -----<>------ */}
+        <ListItem disablePadding>
+          <ListItemButton component="a" href="#simple-list" >
+            <ListItemIcon>
+              <Storefront />
+            </ListItemIcon>
+            <ListItemText primary="MarketPlace" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton component="a" href="#simple-list" >
+            <ListItemIcon>
+              <Person />
+            </ListItemIcon>
+            <ListItemText primary="Friends" />
+          </ListItemButton>
+        </ListItem>
+        {/* -----<>------ */}
+        <ListItem disablePadding>
+          <ListItemButton component="a" href="#simple-list" >
+            <ListItemIcon>
+              <Settings />
+            </ListItemIcon>
+            <ListItemText primary="Settings" />
+          </ListItemButton>
+        </ListItem>
+        {/* -----<>------ */}
+        <ListItem disablePadding>
+          <ListItemButton component="a" href="#simple-list" >
+            <ListItemIcon>
+              <AccountBox />
+            </ListItemIcon>
+            <ListItemText primary="Profile" />
+          </ListItemButton>
+        </ListItem>
+        {/* -----<>------ */}
+        <ListItem disablePadding>
+          <ListItemButton component="a" href="#simple-list" >
+            <ListItemIcon>
+              <ModeNight />
+            </ListItemIcon>
+            <Switch/>
+          </ListItemButton>
+        </ListItem>
+        {/* -----<>------ */}
+        
       </List>
-
-      {/* HomePage
-Pages
-Groups
-NarketPlace
-Friends
-Settiings
-Profile
-switch */}
     </Box>
   );
 };
