8d2f73f65e50a410dbb34a84bfd35ff153df0cfe
devhazemaq <hazemahmedqindeel@gmail.com>
2023-10-02 07:39:30 +0300

    Feed he contain all posts and sohw hem

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,10 +1,18 @@
-import { Box } from '@mui/material'
-import React from 'react'
+import React from "react";
+import {
+  Box,
+} from "@mui/material";
+import Post from "./PostH";
+
 
 const Feed = () => {
   return (
-    <Box flex={4} p={2} bgcolor={'pink'}  >Feed</Box>
-  )
-}
+    <Box flex={4} p={2} >
+      <Post/>
+      <Post/>
+      <Post/>
+    </Box>
+  );
+};
 
-export default Feed
+export default Feed;
