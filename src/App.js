fd6b42258eec2803495c100d2613873263df1610
devhazemaq <hazemahmedqindeel@gmail.com>
2023-10-02 13:50:24 +0300

    add component in app

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
-import { Stack } from "@mui/material";
+import { Box, Stack } from "@mui/material";
 import Feed from "./components/Feed";
 import Rightbar from "./components/Rightbar";
 import Sidebar from "./components/Sidebar";
 import Navbar from "./components/Navbar";
+import Add from "./components/Add";
 
 function App() {
   
   return (
-    <>
+    <Box>
     <Navbar />
-    
+
     <Stack direction="row" justifyContent="space-between" spacing={2} >
       <Sidebar/>
       <Feed/>
       <Rightbar/>
     </Stack>
-    </>
+    
+    <Add/>
+    
+    </Box>
 
   );
 }
