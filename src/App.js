bea7d938424fa8c3ba5cccfd45917bf07c23c7f9
devhazemaq <hazemahmedqindeel@gmail.com>
2023-10-04 01:00:19 +0300

    add theme form mui

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
+import { Box, ThemeProvider, createTheme } from "@mui/material";
 import HomePage from "./pages/HomePage";
+import { useState } from "react";
 
 function App() {
+
+  const [mode, setMode] = useState("dark");
+
+  const darkTheme = createTheme  ({
+    palette: {
+      mode: mode,
+    },
+  });
   return (
-    <div>
-      <HomePage />
-    </div>
+    <ThemeProvider theme={darkTheme}>
+
+    <Box bgcolor={"background.default"} color={"text.primary"}>
+      <HomePage mode={mode} setMode={setMode} />
+    </Box>
+    </ThemeProvider>
   );
 }
 
