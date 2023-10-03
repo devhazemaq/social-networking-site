37cae651ecd5f76b8716fcbed55d3acc249c6a95
devhazemaq <hazemahmedqindeel@gmail.com>
2023-10-04 00:59:44 +0300

    styled for Add

---
+++ src/components/Add/style.js
@@ -0,0 +1,13 @@
+import { Box, Modal, styled } from "@mui/material";
+
+export const StyledModal = styled(Modal)({
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+});
+
+export const UserBox = styled(Box)({
+  display: "flex",
+  alignItems: "center",
+  gap: "10px",
+});
