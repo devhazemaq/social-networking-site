37f16bfe22c73ebdec1e607a59276badfc2612dc
devhazemaq <hazemahmedqindeel@gmail.com>
2023-10-04 00:04:43 +0300

    Rightbar  he contain static data but i improved the code

--- src/components/Rightbar/index.jsx
+++ src/components/Rightbar/index.jsx
@@ -1,10 +1,92 @@
 import React from "react";
-import { Box } from "@mui/material";
+import {
+  Avatar,
+  AvatarGroup,
+  Box,
+  Divider,
+  ImageList,
+  ImageListItem,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+  Typography,
+} from "@mui/material";
+import {
+  avatarUsers,
+  latestConversations,
+  latestPhotos,
+} from "../../constants";
 
 const Rightbar = () => {
   return (
-    <Box sx={{ backgroundColor: "pink" }} flex={2}>
-      Rightbar
+    <Box flex={2} p={2} pt={3} sx={{ display: { xs: "none", sm: "block" } }}>
+      <Box position="fixed" width={300}>
+        <Typography variant="h6" fontWeight={100} mt={2}>
+          Online Friends
+        </Typography>
+
+        <AvatarGroup max={7}>
+          {avatarUsers?.map((item, index) => (
+            <React.Fragment value={index}>
+              <Avatar alt={item.name} src={item.image} />
+            </React.Fragment>
+          ))}
+        </AvatarGroup>
+
+        <Typography variant="h6" fontWeight={100} mt={2} mb={2}>
+          Latest Photos
+        </Typography>
+
+        <ImageList cols={3} rowHeight={100} gap={5}>
+          {latestPhotos?.map((item, index) => (
+            <ImageListItem key={index}>
+              <img src={item.image} alt={item.name} loading="lazy" />
+            </ImageListItem>
+          ))}
+        </ImageList>
+
+        <Typography variant="h6" fontWeight={100} mt={2}>
+          Latest Conversations
+        </Typography>
+
+        <List
+          sx={{
+            width: "100%",
+            maxWidth: 360,
+            bgcolor: "background.paper",
+            paddingTop: "0",
+          }}
+        >
+          {latestConversations?.map((item, index) => (
+            <>
+              <ListItem key={index} alignItems="flex-start">
+                <ListItemAvatar>
+                  <Avatar alt={item.title} src={item.image} />
+                </ListItemAvatar>
+                <ListItemText
+                  primary={item.title}
+                  secondary={
+                    <React.Fragment>
+                      <Typography
+                        sx={{ display: "inline" }}
+                        component="span"
+                        variant="body2"
+                        color="text.primary"
+                      >
+                        {item.message}
+                      </Typography>
+                    </React.Fragment>
+                  }
+                />
+              </ListItem>
+              {index < latestConversations.length - 1 && (
+                <Divider variant="inset" component="li" />
+              )}
+            </>
+          ))}
+        </List>
+      </Box>
     </Box>
   );
 };
