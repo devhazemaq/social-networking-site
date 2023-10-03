f4b173f334b668db161c8ffe5f5e9bf20d3e18fa
devhazemaq <hazemahmedqindeel@gmail.com>
2023-10-03 11:58:52 +0300

    Navbar i but hem AppBar & logo & search & icon % img for user

---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+
+import {
+  StyledSearch,
+  StyledToolbar,
+  SearchIconWrapper,
+  IconB,
+  UserBox,
+} from "./style";
+import {
+  AppBar,
+  Avatar,
+  Badge,
+  Box,
+  InputBase,
+  Typography,
+} from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+import { Mail, Notifications } from "@mui/icons-material";
+
+const Navbar = () => {
+  return (
+    <Box>
+      <AppBar position="static">
+        <StyledToolbar>
+          <Typography variant="h6">Haz Haz</Typography>
+          <StyledSearch>
+            <SearchIconWrapper>
+              <SearchIcon color="disabled" sx={{ marginRight: "50px" }} />
+            </SearchIconWrapper>
+            <InputBase
+              sx={{ marginLeft: "25px", width: "90%" }}
+              placeholder="Search..."
+            />
+          </StyledSearch>
+          <IconB>
+            <Badge badgeContent={4} color="error">
+              <Mail />
+            </Badge>
+            <Badge badgeContent={13} color="error">
+              <Notifications />
+            </Badge>
+            <Avatar src="/assets/images/hhh.jpg" alt="profile img" />
+          </IconB>
+          <UserBox>
+            <Avatar src="/assets/images/hhh.jpg" alt="profile img" />
+            <Typography variant="span">Hazem</Typography>
+          </UserBox>
+        </StyledToolbar>
+      </AppBar>
+    </Box>
+  );
+};
+
+export default Navbar;
