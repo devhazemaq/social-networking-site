368f1f5f5b42b5018857a8bdfc677d3fc8a57c2f
devhazemaq <hazemahmedqindeel@gmail.com>
2023-10-03 13:34:16 +0300

    HomePage he contain many component and they are Navbar , Sidebar, Feed, Rightbar

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,13 +1,21 @@
-import React from 'react'
-import Navbar from '../../components/Navbar'
+import React from "react";
+import Navbar from "../../components/Navbar";
+import Sidebar from "../../components/Sidebar"
+import Feed from "../../components/Feed";
+import Rightbar from "../../components/Rightbar";
+import { Stack } from "@mui/material";
 
 const HomePage = () => {
   return (
     <>
-      <Navbar />
-      
+      <Navbar/>
+      <Stack direction="row" spacing={2} justifyContent="space-between" >
+        <Sidebar/>
+        <Feed />
+        <Rightbar />
+      </Stack>
     </>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
