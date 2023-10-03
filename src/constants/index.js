2e42f280cdece6d80818e0a2c02508c4bd4bede4
devhazemaq <hazemahmedqindeel@gmail.com>
2023-10-03 13:26:49 +0300

    constants he contain menuItemsForSidebar he contain icon and text

---
+++ src/constants/index.js
@@ -0,0 +1,20 @@
+import {
+  AccountBox,
+  Article,
+  Group,
+  Home,
+  Person,
+  Settings,
+  Storefront,
+} from "@mui/icons-material";
+
+
+export const menuItemsForSidebar = [
+  { icon: <Home />, text: "Homepage" },
+  { icon: <Article />, text: "Pages" },
+  { icon: <Group />, text: "Groups" },
+  { icon: <Storefront />, text: "MarketPlace" },
+  { icon: <Person />, text: "Friends" },
+  { icon: <Settings />, text: "Settings" },
+  { icon: <AccountBox />, text: "Profile" },
+];
